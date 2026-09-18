9f03b64dd4f397a0fb7d632062c22e292acf16fc
cristofer <cristoferrodriguez610@gmail.com>
2026-09-18 00:53:55 -0600

    feat: actualizar clases de Forma 03 para Karol Molina

---
+++ src/data/forma03Data.js
@@ -0,0 +1,103 @@
+export const FORMA03_BY_STUDENT = {
+  // Karol Elissa Molina Martinez
+  '20241002333': {
+    classes: [
+      {
+        code: 'II411',
+        name: 'ORGANIZACION Y METODOS',
+        section: '0701',
+        hi: '0700',
+        hf: '0800',
+        days: 'MaMiJuVi',
+        building: 'B2',
+        classroom: '204',
+        uv: '4.00',
+        period: '3',
+        modality: 'Presencial'
+      },
+      {
+        code: 'FF201',
+        name: 'LOGICA MATEMATICA',
+        section: '1100',
+        hi: '1100',
+        hf: '1200',
+        days: 'LuMaMi',
+        building: 'F1',
+        classroom: '302',
+        uv: '3.00',
+        period: '3',
+        modality: 'Presencial'
+      },
+      {
+        code: 'IE221',
+        name: 'CIRCUITOS ELECTRICOS',
+        section: '1400',
+        hi: '1400',
+        hf: '1500',
+        days: 'LuMaMiJuVi',
+        building: 'B2',
+        classroom: '405 MESAS DE DIBUJO',
+        uv: '5.00',
+        period: '3',
+        modality: 'Presencial'
+      }
+    ],
+    labs: []
+  },
+  // Luz Alicia Molina Martinez
+  '20211000375': {
+    classes: [
+      {
+        code: 'DQ101',
+        name: 'DIBUJO I',
+        section: '0700',
+        hi: '0700',
+        hf: '1000',
+        days: 'LuMi',
+        building: 'D1',
+        classroom: '313(PARA DIBUJO)',
+        uv: '2.00',
+        period: '2',
+        modality: 'Presencial'
+      },
+      {
+        code: 'FS200',
+        name: 'FISICA GENERAL II',
+        section: '1000',
+        hi: '1000',
+        hf: '1100',
+        days: 'LuMaMiJuVi',
+        building: 'E1',
+        classroom: '302',
+        uv: '5.00',
+        period: '2',
+        modality: 'Presencial'
+      },
+      {
+        code: 'MM314',
+        name: 'PROGRAMACION',
+        section: '1400',
+        hi: '1400',
+        hf: '1500',
+        days: 'LuMaMi',
+        building: 'C3',
+        classroom: '314',
+        uv: '3.00',
+        period: '2',
+        modality: 'Presencial'
+      }
+    ],
+    labs: [
+      {
+        code: 'FS200',
+        name: 'FISICA GENERAL II',
+        section: '1500',
+        hi: '1500',
+        hf: '1700',
+        days: 'Mi',
+        building: 'E1',
+        classroom: '302'
+      }
+    ]
+  }
+};
